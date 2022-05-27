b94cf392c0f67ab62edec6fec453ee1e9b4b7848
Noah Cairns <noah.cairns1@gmail.com>
2022-05-27 14:04:57 -0400

    motion

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,12 +1,15 @@
 import { Footer } from "./Footer";
 import { NavBar } from "./NavBar";
+import { motion } from "framer-motion";
 
 const Layout = ({ props, children }) => {
 	return (
 		<div className="relative min-h-screen">
 			<div className="pb-[4.75rem]">
 				<NavBar />
+
 				{children}
+
 				<Footer />
 			</div>
 		</div>
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,20 @@
 import "../styles/globals.css";
 import { TTTContext, TTTWrapper } from "../contexts/TTTContext";
-import {AuthWrapper} from "../contexts/AuthContext";
+import { AuthWrapper } from "../contexts/AuthContext";
 import { Layout } from "../components/Layout";
+import { AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<AuthWrapper>
-				<Layout>
-					<TTTWrapper>
-						<Component {...pageProps} />
-					</TTTWrapper>
-				</Layout>
+
+					<Layout>
+						<TTTWrapper>
+							<Component {...pageProps} />
+						</TTTWrapper>
+					</Layout>
+
 			</AuthWrapper>
 		</>
 	);
